e73385957765be53f8ac5fc9fa8b51613fcef03c
pif128 <pif128@hotmail.com>
2022-10-18 01:18:22 +0300

    GetByIdTecnolgyQuery Test Commit

---
+++ src/pifProjects/pif.Kodlama.io.Devs/pif.Kodlama.io.Devs.Application/Features/Technologies/Queries/GetByIdTechnology/GetByIdTechnologyQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pif.Kodlama.io.Devs.Application.Features.Technologies.Queries.GetByIdTechnology
+{
+	public class GetByIdTechnologyQuery
+	{
+	}
+}
